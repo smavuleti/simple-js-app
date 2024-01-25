279d093cf6ff2a508de2e2c54512811ffa4d7d77
Sruthi Mavuleti <sruthi.mavuleti@gmail.com>
2024-01-25 00:51:58 -0800

    Removed previous code and added an array list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
-alert("Hello World!")
-let favoriteFood="Pizza";
-console.log("Gone through Javascript file");
-document.write("My Favorite Food is " +favoriteFood);
+var pokemonList = [];
+var pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        type: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 6,
+        type: ['fire']
+    },
+    {
+        name: 'Charmeleon',
+        height: 1,
+        type: ['fire']
+    }
+]
+//console.log(pokemonList[0].name);
+document.write("First array name is " + pokemonList[0].name + ", height is " + pokemonList[0].height + ", and type is " + pokemonList[0].type);
