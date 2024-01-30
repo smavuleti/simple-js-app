a1847b6781255e33f480a380de2a72d58e100869
Sruthi Mavuleti <sruthi.mavuleti@gmail.com>
2024-01-30 10:52:46 -0800

    Added an item to pokemanList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,71 @@
-var pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 7,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Ivysaur',
-        height: 9,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Venusaur',
-        height: 2,
-        type: ['grass', 'poison']
-    },
-    {
-        name: 'Charmander',
-        height: 6,
-        type: ['fire']
-    },
-    {
-        name: 'Charmeleon',
-        height: 1,
-        type: ['fire']
-    }
-]
-/* For Example: 
-pokemonList.forEach( (item,i,arr) => console.log(i, item, arr));
-*/
+let pokemonRepository = (function () {
+    let pokemanList = [
+        {
+            name: 'Bulbasaur',
+            height: 7,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Ivysaur',
+            height: 9,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Venusaur',
+            height: 2,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 6,
+            type: ['fire']
+        },
+        {
+            name: 'Charmeleon',
+            height: 1,
+            type: ['fire']
+        }
+    ]
 
+    function getAll() {
+        return pokemanList;
+    }
+    function add(pokeman) {
+        pokemanList.push(pokeman);
+    }
+    return {
+        getAll: getAll,
+        add: add
+    }
+})();
+
+console.log(pokemonRepository.getAll());
+//adding a single item to the pokemonList array
+pokemonRepository.add({
+    name: 'Pikachu',
+    height: 8,
+    type: ['grass', 'poison']
+});
+console.log(pokemonRepository.getAll());
+
+// creating a new variable to store the maximum height in pokeman array 
 var maxHeight = 0;
-pokemonList.forEach(function (checkMaxHeight){
+//forEach() loop to search and check maximum height in array 
+pokemonRepository.getAll().forEach(function (checkMaxHeight) {
+    //if condition is to compare the each height in array to maxheight
     if (checkMaxHeight.height > maxHeight) {
+        //storing the maximum height in maxHeight
         maxHeight = checkMaxHeight.height;
     }
 });
-
-pokemonList.forEach(function (details){
+//forEach() to display pokeman list array 
+pokemonRepository.getAll().forEach(function (details) {
+    //if condition is to display array with maximum height text
     if (details.height == maxHeight) {
         document.write(details.name + " (height: " + details.height + ")- Wow that\'s big!<br> <br>");
     } else {
         document.write(details.name + " (height: " + details.height + ") <br><br>");
     }
 });
-
-
 
 
