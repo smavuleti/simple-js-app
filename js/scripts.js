f13e0ecb0c522280add7e49dc15c52ddb92caf8b
Sruthi Mavuleti <sruthi.mavuleti@gmail.com>
2024-02-06 01:48:04 -0800

    Replaced static data with API data and displayed the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,130 +1,104 @@
 let pokemonRepository = (function () {
-    let pokemonList = [
-        {
-            name: 'Bulbasaur',
-            height: 7,
-            type: ['grass', 'poison']
-        },
-        {
-            name: 'Ivysaur',
-            height: 9,
-            type: ['grass', 'poison']
-        },
-        {
-            name: 'Venusaur',
-            height: 2,
-            type: ['grass', 'poison']
-        },
-        {
-            name: 'Charmander',
-            height: 6,
-            type: ['fire']
-        },
-        {
-            name: 'Charmeleon',
-            height: 1,
-            type: ['fire']
+    let pokemonList = [];
+    //fetching Pokemon details from API limited to 10 records
+    let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=10";
+
+
+    function add(pokemon) {
+        //condition to check the keys from API to add each pokemon to pokemon list
+        if (typeof pokemon === "object" && "name" in pokemon && "detailsUrl" in pokemon
+        ) {
+            pokemonList.push(pokemon);
+        } else {
+            console.log("Pokemon is not found");
         }
-    ]
+
+    }
 
     function getAll() {
         return pokemonList;
     }
-    /*
-    function add(pokemon) {
-         //validating typeof parameter 
-        if (typeof pokemon == 'object') {
-            pokemonList.push(pokemon);
-        }
-    }
-    */
-    function add(pokemon) {
-        //validating Object.keys() of the parameter are equal to the specific keys
-        if (typeof pokemon.name == 'string' && typeof pokemon.height == 'number' && typeof pokemon.type == 'object') {
-            pokemonList.push(pokemon);
-        }
-    }
 
-    function showDetails(pokemon) {
-        console.log(pokemon);
-    }
 
-    //DOM: function to add buttons for each pokemon in the array list
+
+
     function addListItem(pokemon) {
         let pokemonList = document.querySelector(".pokemon-list");
-
         let listItem = document.createElement("li");
-
         let button = document.createElement("button");
         button.innerText = pokemon.name;
         button.classList.add("pokemon-button");
-        
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);
-
-        //Event listener function to the buttons in the pokemon array list
         checkButtonEvent(button, pokemon);
 
-        /*
-        //event listener for button click action
-        button.addEventListener('click', function (event){
-            console.log (event);
-            alert("Button is clicked!");
-            showDetails(pokemon);
-            
-        });*/
+
     }
 
-    // event listener function to check if and which button is clicked in the array list
+
+    // funtion loadlist to fetch the list of pokemon list from API
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+                //console.log(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
+    // funtion loaddetails to fetch the list of pokemon details from pokemon list
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+
+    }
     function checkButtonEvent(button, pokemon) {
         button.addEventListener('click', function (event) {
             console.log(event);
-
-            alert(pokemon.name+ " Pokemon button is clicked!");
-            //alert("Button is clicked!");
+            alert(pokemon.name + " is clicked!");
             showDetails(pokemon);
         });
     }
 
+    //load pokemon details from API 
+    function showDetails(item) {
+        pokemonRepository.loadDetails(item).then(function () {
+            console.log(item);
+        });
+    }
 
     return {
-        getAll: getAll,
         add: add,
-        addListItem: addListItem
-    }
+        getAll: getAll,
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails
+
+    };
 })();
 
-console.log(pokemonRepository.getAll());
-//adding a single item to the pokemonList array
-pokemonRepository.add({
-    name: 'Pikachu',
-    height: 8,
-    type: ['grass', 'poison']
+//forEach() to display pokemon list  
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
-console.log(pokemonRepository.getAll());
 
-// creating a new variable to store the maximum height in pokemon array 
-var maxHeight = 0;
-//forEach() loop to search and check maximum height in array 
-pokemonRepository.getAll().forEach(function (checkMaxHeight) {
-    //if condition is to compare the each height in array to maxheight
-    if (checkMaxHeight.height > maxHeight) {
-        //storing the maximum height in maxHeight
-        maxHeight = checkMaxHeight.height;
-    }
-});
-//forEach() to display pokemon list array 
-pokemonRepository.getAll().forEach(function (details) {
-    pokemonRepository.addListItem(details);
-
-    /* 
-    //if condition is to display array with maximum height text
-    if (details.height == maxHeight) {
-        document.write(details.name + " (height: " + details.height + ")- Wow that\'s big!<br> <br>");
-    } else {
-        document.write(details.name + " (height: " + details.height + ") <br><br>");
-    }
-    */
-
-});
 
